f9b106f17f218059148afbfb3f92fbbbaa7dc5e3
FrozenMind <valentin.rebscher@live.de>
2017-07-09 17:52:53 +0200

    fix: all notes got read now and have an id

--- src/app/overview.component.ts
+++ src/app/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
 import { Note } from './notes';
 import { MainService } from './main.service';
@@ -12,11 +13,17 @@ export class OverviewComponent implements OnInit {
   title: string = "Overview of your notes";
   notes: Note[];
 
-  constructor(private mainService: MainService) { }
+  constructor(
+    private mainService: MainService,
+    private router: Router) { }
 
   ngOnInit(): void {
     console.log("Overview ngOnInit")
     this.mainService.getNotes()
       .then(notes => this.notes = notes);
+  }
+
+  gotoEdit(note): void {
+    this.router.navigate(['/edit', note.id])
   }
 }
