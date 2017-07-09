1ae4919eea519a8cb1e66bf7226b59d4e1ecfb8c
FrozenMind <valentin.rebscher@live.de>
2017-07-09 15:25:50 +0200

    add: start screen that displays all notes

--- src/app/overview.component.ts
+++ src/app/overview.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Note } from './notes';
+import { MainService } from './main.service';
 
 @Component({
   selector: 'overview-notes',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  title: string = "Overview of your notes";
+  notes: Note[];
 
+  constructor(private mainService: MainService) { }
+
+  ngOnInit(): void {
+    console.log("Overview ngOnInit")
+    this.mainService.getNotes()
+      .then(notes => this.notes = notes);
+  }
 }
