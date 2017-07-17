1117bf684ce5d4d11f47aacbd34355997569bdb7
FrozenMind <valentin.rebscher@live.de>
2017-07-17 20:00:30 +0200

    remove: main menu bar

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -1,62 +1,58 @@
-// src/electron/electron.js
+//src/electron/electron.js
 
 const {
   app,
   BrowserWindow
 } = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+//Keep a global reference of the window object, if you don't, the window will
+//be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow() {
-  // Create the browser window.
+  //Create the browser window.
   win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    frame: false //remove frames, menu bar, etc.
   })
 
-  // and load the index.html of the app.
+  //load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
+  //Open the DevTools with next line (can be done always by pressing ctrl+alt+i)
   //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    //Dereference the window object, usually you would store windows
+    //in an array if your app supports multi windows, this is the time
+    //when you should delete the corresponding element.
     win = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+//This method will be called when Electron has finished
+//initialization and is ready to create browser windows.
+//Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
-//create http server
+
+//all the http server stuff TODO new file pls
 var express = require('express')
 var server = express()
 var cors = require('cors')
